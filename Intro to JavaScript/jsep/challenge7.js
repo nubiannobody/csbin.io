b6587fdeefc961e16e75ed8d62c8d05625bd1c81
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-09-28 21:43:05 -0700

    Add completed exercise

---
+++ Intro to JavaScript/jsep/challenge7.js
@@ -0,0 +1,20 @@
+/* =========== *
+* Challenge 7 *
+* ============ */
+
+// Create a function isPrime that console.logs a boolean indicating if `number` is prime or not. 
+
+// A prime number is a whole number greater than 1, with only two factors itself and 1.
+// Example prime number 13, divisible by 1 and 13
+// 15 is not a prime number as it is divisible by 3 & 5
+
+function isPrime(number) {
+
+}
+
+     console.log(isPrime(-7)); // => false
+     console.log(isPrime(2)); // => true
+     console.log(isPrime(11)); // => true
+     console.log(isPrime(15)); // => false
+     
+     
